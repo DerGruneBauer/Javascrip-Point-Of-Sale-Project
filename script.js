5f2236e3a0c80ab1c57b096abeba554414efa1af
remington <remigreenbauer@gmail.com>
2020-12-10 19:24:08 -0500

    added descriptions

--- script.js
+++ script.js
@@ -17,9 +17,12 @@ const cafeItems = [
 ]
 
 let image = document.querySelectorAll('img');
+let deleteButton = document.querySelectorAll('.delete');
 let cart = [];
 let totalCost = 0;
 
+
+//Add to cart function
 image.forEach((image) => {
     image.addEventListener("click", (addToCart) => {
         //listen for click on specific image. collect ID/index number and return the object at that index to cart
@@ -40,8 +43,32 @@ image.forEach((image) => {
         checkout.appendChild(newCartItem);
 
         totalCost = cafeItems[idNum].price + totalCost;
-        cost.innerHTML = (`The total cost will be ${totalCost}`);
+        cost.innerHTML = (`<strong>Total Cost: </strong>${totalCost}`);
 
     })
 })
 
+//Removal of items from cart
+
+deleteButton.forEach((deleteButton) => {
+    deleteButton.addEventListener('click', (removeFromCart) => {
+
+    })
+})
+
+
+//Additions of names, descriptions and prices
+document.querySelector('.dripCoffeeDes').innerHTML = `${cafeItems[0].name}<br>${cafeItems[0].description}<br>${cafeItems[0].price}`;
+document.querySelector('.espressoDes').innerHTML = `${cafeItems[1].name}<br>${cafeItems[1].description}<br>${cafeItems[1].price}`;
+document.querySelector('.americanoDes').innerHTML = `${cafeItems[2].name}<br>${cafeItems[2].description}<br>${cafeItems[2].price}`;
+document.querySelector('.latteDes').innerHTML = `${cafeItems[3].name}<br>${cafeItems[3].description}<br>${cafeItems[3].price}`;
+document.querySelector('.cappucinoDes').innerHTML = `${cafeItems[4].name}<br>${cafeItems[4].description}<br>${cafeItems[4].price}`;
+document.querySelector('.teaDes').innerHTML = `${cafeItems[5].name}<br>${cafeItems[5].description}<br>${cafeItems[5].price}`;
+document.querySelector('.hotChocoDes').innerHTML = `${cafeItems[6].name}<br>${cafeItems[6].description}<br>${cafeItems[6].price}`;
+document.querySelector('.mochaDes').innerHTML = `${cafeItems[7].name}<br>${cafeItems[7].description}<br>${cafeItems[7].price}`;
+document.querySelector('.icedTeaDes').innerHTML = `${cafeItems[8].name}<br>${cafeItems[8].description}<br>${cafeItems[8].price}`;
+document.querySelector('.waterDes').innerHTML = `${cafeItems[9].name}<br>${cafeItems[9].description}<br>${cafeItems[9].price}`;
+document.querySelector('.mateDes').innerHTML = `${cafeItems[10].name}<br>${cafeItems[10].description}<br>${cafeItems[10].price}`;
+document.querySelector('.saladDes').innerHTML = `${cafeItems[11].name}<br>${cafeItems[11].description}<br>${cafeItems[11].price}`;
+document.querySelector('.soupDes').innerHTML = `${cafeItems[12].name}<br>${cafeItems[12].description}<br>${cafeItems[12].price}`;
+document.querySelector('.pastryDes').innerHTML = `${cafeItems[13].name}<br>${cafeItems[13].description}<br>${cafeItems[13].price}`;
